96ab44cebbc7c31a697df90c9f38af2c2e39ed38
jmjo <jmjo@rydis.co.kr>
2023-04-20 12:25:51 +0900

    템플릿 삭제

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,20 +5,6 @@ from django.urls import path, include
 from webapp import views
 
 urlpatterns = [
-
-    # path('index', views.index),
-    path('setting_complete', views.setting_complete),
-    path('process', views.process),
-    path('process2', views.process2),
-    path('product_list', views.product_list),
-
-    path('index', views.index),
-    path('fileupload/', views.FileFieldView.as_view(), name='fileupload'),  # 컨텐츠 등록
-    path('fileupload2/', views.ProductSaveView.as_view()),
-    path('content/', views.ContentLoadAPI.as_view()),
-
-    # path('content/', views.ContentLoadAPI.as_view()),
-
     # g식 관리자 페이지
     path('g_project', views.g_project),
 ]
